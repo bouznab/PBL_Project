5c24edd90367022fb390c59af54e40f4574dee1c
tobfisch <tobias.fischer1@tu-dresden.de>
2018-01-20 17:41:33 +0100

    ping.py is running on all hosts. Python package "netifaces" has to be installed!!

--- ping.py
+++ ping.py
@@ -3,23 +3,20 @@
 from scapy.all import *
 from time import *
 import sys
+import netifaces 
 
 ipToMac = {"10.0.0.1":"10:10:10:10:10:11","10.0.0.2":"10:10:10:10:10:12",
         "10.0.0.3":"10:10:10:10:10:13","10.0.0.4":"10:10:10:10:10:14"}
+ipToIface={"10.0.0.1":"h1-eth2","10.0.0.2":"h2-eth2","10.0.0.3":"h3-eth2","10.0.0.4":"h4-eth2"}
 
-def QoS_ping(host, port, count=100):
+def QoS_ping(host, port, iface, count=100):
     packet = Ether(dst=ipToMac[host])/IP(dst=host)/TCP(sport=port, dport=port)
     t = 0.0
     average = 0.0
-    #s = conf.L3socket(iface='h1-eth2')
     for x in range(count):
         t = 0.0
         t1=time()
-        #ans=s.sr1(packet, verbose=0)
-        ans = srp(packet,iface="h1-eth2",verbose=0)
-        #rx=ans[0][1]
-        #tx=ans[0][0]
-        #delta = rx.time -tx.sent_time
+        ans = srp(packet,iface=iface,verbose=0)
         t2=time()
         t+=t2-t1
         average += t
@@ -29,7 +26,8 @@ def QoS_ping(host, port, count=100):
     #s.close()
 
 if __name__=="__main__":
-    if len(sys.argv) < 1:
-        QoS_ping('10.0.0.3', 10022)
+    interfaces = netifaces.interfaces()
+    if len(sys.argv) <= 1:
+        QoS_ping('10.0.0.3', 10022, interfaces[1])
     else: 
-        QoS_ping(sys.argv[1], int(sys.argv[2]))
+        QoS_ping(sys.argv[1], int(sys.argv[2]),interfaces[1])
