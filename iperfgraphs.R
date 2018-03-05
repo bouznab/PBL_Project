15a58dc7d542fcfeee2cf8c56e0d5d462cb5a367
Tobias Fischer <tobias.fischer1@tu-dresden.de>
2018-03-05 13:28:41 +0100

    new throughput graph, iperfgraphs.R plots both graphs at once

--- iperfgraphs.R
+++ iperfgraphs.R
@@ -2,9 +2,16 @@ library(lattice)
 
 csvDataFrame <- read.table("iperf_plot1.csv",header=TRUE)
 
-trellis.device("svg", file="iperf.svg", color=T, width=6.5, height=5.0)
+trellis.device("svg", file="throughput.svg", color=T, width=6.5, height=5.0)
+
+xyplot(throughput ~ time, group=port, csvDataFrame, type=c("p","g","o"), 
+       pch=20, auto.key=TRUE, xlab="time[s]", ylab="throughput [Kbit/s]")
+
+dev.off() -> null
+
+trellis.device("svg", file="retry.svg", color=T, width=6.5, height=5.0)
 
 xyplot(retry ~ time, group=port, csvDataFrame, type=c("p","g","o"), 
-       pch=20, auto.key=TRUE, xlab="time[s]", ylab="# retries")
+       pch=20, auto.key=TRUE, xlab="time[s]", ylab="# retrires")
 
 dev.off() -> null
